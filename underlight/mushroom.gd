392c77f379998e3e803a02ac5ca8283583108187
Emiel Kalksma <emielk94@gmail.com>
2026-01-21 15:57:57 -0300

    Add damage number script

--- underlight/mushroom.gd
+++ underlight/mushroom.gd
@@ -1,1 +1,11 @@
 extends CharacterBody2D
+
+@onready var dmg_num_origin = $dmg_num_origin
+var health = 100
+
+func _ready():
+	take_damage(10)
+	
+func take_damage(damage):
+	health -= damage
+	DamageNumbers.display_number(damage, dmg_num_origin.global_position)
--- underlight/scenes/camera_2d.gd
+++ underlight/scenes/camera_2d.gd
@@ -1,6 +1,6 @@
 extends Camera2D
 
-@export var zoom_in := 2
+@export var zoom_in := 1.5
 
 func _ready():
 	zoom = Vector2(zoom_in,zoom_in)
