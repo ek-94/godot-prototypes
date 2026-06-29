c5a38d6ea97d7eacc7c53612ee78c0863389c5f3
ek-94 <emielk94@gmail.com>
2026-06-28 22:53:45 -0300

    Add dead state

--- zombie-survival/scenes/dead.gd
+++ zombie-survival/scenes/dead.gd
@@ -1,5 +1,6 @@
 extends State
 
 func enter():
+	actor.velocity = Vector3.ZERO
 	actor.is_dead = true
 	actor.turn_on_ragdoll()
--- zombie-survival/scripts/zombie.gd
+++ zombie-survival/scripts/zombie.gd
@@ -9,7 +9,7 @@ extends CharacterBody3D
 @onready var navigation_agent_3d: NavigationAgent3D = $NavigationAgent3D
 @onready var animation_player: AnimationPlayer = $visuals/erika/AnimationPlayer
 @onready var visuals: Node3D = $visuals
-@onready var physical_bone_simulator_3d: PhysicalBoneSimulator3D = $visuals/Sketchfab_Scene/Sketchfab_model/fbx_merge_fbx/Object_2/RootNode/Root/Object_5/Skeleton3D/PhysicalBoneSimulator3D
+@onready var physical_bone_simulator_3d: PhysicalBoneSimulator3D = $visuals/erika/Skeleton3D/PhysicalBoneSimulator3D
 @onready var health_bar: ProgressBar = $ProgressBar
 
 @export var run_speed = 10
@@ -37,7 +37,7 @@ func take_damage(dmg):
 		health -= dmg
 		health_bar.value = health
 		if health <= 0:
-			die()
+			state_machine.change_state("Dead")
 		
 func die():
 	is_dead = true
@@ -50,10 +50,10 @@ func turn_on_ragdoll():
 	physical_bone_simulator_3d.physical_bones_start_simulation()
 	physical_bone_simulator_3d.active = true
 	
-	for bone in physical_bone_simulator_3d.get_children():
-		if bone is PhysicalBone3D:
-			bone.linear_velocity = velocity
-			bone.apply_central_impulse(velocity * 0.2)
+	#for bone in physical_bone_simulator_3d.get_children():
+		#if bone is PhysicalBone3D:
+			#bone.linear_velocity = velocity
+			#bone.apply_central_impulse(velocity * 0.2)
 	
 func _physics_process(delta: float) -> void:
 		if not is_on_floor():
